2e3b91d4b5b497c7b2bc7c1f9ee991c264f3cc91
Affteron <Lontaredal@outlook.com>
2022-01-08 13:15:29 +0300

    add fragment shader for black background

---
+++ programs/fragment_shader.glsl
@@ -0,0 +1,11 @@
+#version 460
+// The only task of the fragment shader is to compute the color
+// of each pixel.
+
+out vec4 fragColor; 
+
+void main() {
+    vec3 col = vec3(0.0);
+
+    fragColor = vec4(col, 1.0);
+}
