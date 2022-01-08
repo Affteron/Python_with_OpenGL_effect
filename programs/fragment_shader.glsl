b980f977e99d8f422c13c1afec2afa03a1f1f9ed
Affteron <Lontaredal@outlook.com>
2022-01-08 17:06:14 +0300

    Add simple fragment shader

--- programs/fragment_shader.glsl
+++ programs/fragment_shader.glsl
@@ -9,7 +9,10 @@ uniform float time;
 
 
 void main() {
+    vec2 uv = (gl_FragCoord.xy - 0.5 * resolution.xy) / resolution.y;
     vec3 col = vec3(0.0);
+
+    col += 0.01 / length(uv);
 
     fragColor = vec4(col, 1.0);
 }
