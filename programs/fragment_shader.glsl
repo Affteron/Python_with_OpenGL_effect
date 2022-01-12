a5b96935e6a38c32e2199eab9d3f47aa663ed601
Affteron <Lontaredal@outlook.com>
2022-01-12 12:51:13 +0300

    Creation cardioid

--- programs/fragment_shader.glsl
+++ programs/fragment_shader.glsl
@@ -12,7 +12,17 @@ void main() {
     vec2 uv = (gl_FragCoord.xy - 0.5 * resolution.xy) / resolution.y;
     vec3 col = vec3(0.0);
 
-    col += 0.01 / length(uv);
+    // Const for calculate normilize x coordinate 
+    float r = 0.17;
+
+    for (float i = 0.0; i < 60.0; i++){
+        float a = i / 3;
+        float dx = 2 * r * cos(a) - r * cos(2 * a);
+        float dy = 2 * r * sin(a) - r * sin(2 * a);
+
+        col += 0.001 / length(uv - vec2(dx + 0.1, dy));
+    }
+
 
     fragColor = vec4(col, 1.0);
 }
